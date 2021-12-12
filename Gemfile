b0c9aeb2a60b0087d605440585ad4e1b5bbe11fa
Vinicius Lins <vlinsufba@gmail.com>
2021-12-12 19:03:25 -0300

    Add simplecov

--- Gemfile
+++ Gemfile
@@ -61,5 +61,4 @@ group :development do
   gem 'spring'
 end
 
-# Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
+gem 'simplecov', require: false, group: :test
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,10 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
+
+require 'simplecov'
+SimpleCov.start
+
 require 'rspec/rails'
 require 'support/factory_bot'
 require 'devise'
